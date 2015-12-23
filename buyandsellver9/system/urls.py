94b82500be6320827594ea376b3e1454d6351273
remarcbalisi <remarcbalisi@gmail.com>
2015-12-23 19:35:14 +0800

    remove url ibs

--- buyandsellver9/system/urls.py
+++ buyandsellver9/system/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^ibs/$', views.index, name='index'),
+    url(r'', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^ibs/register/$', views.user_add, name='user_add'),
     url(r'^ibs/login/$', views.user_login, name='user_login'),
