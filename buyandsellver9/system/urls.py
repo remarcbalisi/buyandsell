49be3d328296c4b2ba49b6de8bc641f6712ab715
remarcbalisi <remarcbalisi@gmail.com>
2015-12-24 20:01:02 +0800

    urls.py: change home url to index

--- buyandsellver9/system/urls.py
+++ buyandsellver9/system/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/$', views.index, name='index'),
+    url(r'^index/$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^register/$', views.user_add, name='user_add'),
     url(r'^login/$', views.user_login, name='user_login'),
